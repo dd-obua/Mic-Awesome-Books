1289121a804363d6d01ccc7a36a54624c65af8e9
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-14 17:25:49 +0300

    Select add button title and author from html

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const books = [];
-function addBook(title,author){
-    this.title = title;
-    this.author = author;
-return `"${this.title}" by ${this.author} `;
+
+function addBook(title, author) {
+  this.title = title;
+  this.author = author;
+  return `"${this.title}" by ${this.author} `;
 }
+
+const addBtn = document.querySelector('.add-btn');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
