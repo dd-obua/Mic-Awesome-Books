aed93fcc9ee3c0dd115d0f7894e98ab165dba0f7
FilbertKimasa <filbertdan67@gmail.com>
2023-08-15 18:40:25 +0300

    Modify script.js file

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const pAuthor = document.createElement('p');
 const btnAdd = document.querySelector('.add-btn');
 const bookTitleInput = document.querySelector('.title');
 const bookAuthorInput = document.querySelector('.author');
+let card;
 
 let books = [];
 
@@ -19,12 +20,12 @@ const AddBook = () => {
   // this.location.reload();
 };
 
-const updateStorage=()=>{
+const updateStorage = () => {
   localStorage.setItem('books', JSON.stringify(books));
 };
 
-const removeBook = title => {
-  books = books.filter(book => book.title !== title);
+const removeBook = (title) => {
+  books = books.filter((book) => book.title !== title);
   updateStorage();
 };
 
@@ -33,9 +34,11 @@ btnAdd.addEventListener('click', AddBook);
 if (localStorage.length > 0) {
   const bookStore = localStorage.getItem('books');
   books = JSON.parse(bookStore);
-  booksWrapper.innerHTML = `${books.map(book =>
+  booksWrapper.innerHTML = `${books
+    .map(
+      (book) =>
         `<li class="book-card">
-        <p>${book.bookTitle}</p>
+        <p class="title">${book.bookTitle}</p>
         <p>${book.bookAuthor}</p>
         <button class="remove-btn">Remove</button>
       </li>
@@ -44,10 +47,17 @@ if (localStorage.length > 0) {
     .join('')}`;
 }
 
-const removeBtn = document.querySelector('.remove-btn');
-console.log(removeBtn);
-removeBtn.addEventListener('click', removeBook);
+const removeBtns = document.querySelectorAll('.remove-btn');
+// console.log(removeBtn);
+// removeBtn.addEventListener('click', removeBook);
+removeBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    card = e.target.closest('.book-card');
+    console.log(card);
+    // removeBook();
+  });
+});
 
-
-
-
+// removeBtn.addEventListener('click', () => {
+//   console.log('clicked');
+// });
